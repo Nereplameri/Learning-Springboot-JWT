3c397bc330fc0890b5228eb101f3fadc569f1ca6
Alperen <alperenavci2002@gmail.com>
2025-10-23 14:43:19 +0300

    Embed Map in Token

--- jwt/src/main/java/com/alperenavci/jwt/JwtService.java
+++ jwt/src/main/java/com/alperenavci/jwt/JwtService.java
@@ -26,28 +26,51 @@ public class JwtService {
 	public String generateToken(UserDetails userDetails) {
 		
 		//Token 'e Map eklemeye örnek:
-		//Map<String, String> claimsMap = new HashMap<>();
-		//claimsMap.put("role", "ADMIN");
+		Map<String, Object> claimsMap = new HashMap<>();
+		claimsMap.put("role", "ADMIN");
+		
 		
 		// Jwts: dependency ile gelen bir sınıf
 		// builder, sınıfın nesnesini oluşturur. 
 		// Diğer metotlar sürekli kendi sınıfını döndüğü için zincirleme set yapılıyor.
 		return Jwts.builder()
 		.setSubject(userDetails.getUsername()) // Token içindeki Username 'yi set eder.
-		//.setClaims(claimsMap) // Token 'e map eklemek
+		.addClaims(claimsMap) // Token 'e map eklemek
 		.setIssuedAt(new Date()) // Tokenin oluşturulma zamanına erişip Jwts 'ye atar.
 		.setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*2)) // Token bitiş süresidir. milisaniye cinsinden girilir.
 		.signWith(getKey(), SignatureAlgorithm.HS256) //Token oluşurken ve çözerken kullanılan bir key 'e ihtiyaç var.
 		.compact();
 	}
 	
-	//Token çözer
-	// Function<> sınıfı, bir metodun pointerini verip, kod içinde sonradan kullanmak diye düşün.
-	public <T> T exportToken(String token, Function<Claims, T> claimsFunction) {
+	public void test() {
+		//suni bir test sınıfıdır.
+		String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbHBlcmVuIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzYxMjE4NTgyLCJleHAiOjE3NjEyMjU3ODJ9.eDTmHoNF2UykQBvCjca3xFSBC_hfu36SaHHZLMo-fek";
+		String key = "role";
+		Object value= getClaimsByKey(token, key);
+		System.out.println(value);
+	}
+	
+	// Token + token içi istenilen JSON map 'ı verildiğinde key 'i veriyor
+	public Object getClaimsByKey(String token, String key) {
+		Claims claims = getClaims(token);
+		return claims.get(key);
+		
+	}
+	
+	// Token 'den body kısmını çeker.
+	public Claims getClaims(String token) {
 		Claims claims = Jwts.parserBuilder()
 		.setSigningKey(getKey()) // Key ile çözme işlemi
 		.build() // set zincirini kırıp bir alttaki koda uygun return yapıyor.
 		.parseClaimsJws(token).getBody(); // Tokenin body 'sini alır.
+		
+		return claims;
+	}
+	
+	//Token çözer
+	// Function<> sınıfı, bir metodun pointerini verip, kod içinde sonradan kullanmak diye düşün.
+	public <T> T exportToken(String token, Function<Claims, T> claimsFunction) {
+		Claims claims = getClaims(token);
 		
 		return claimsFunction.apply(claims); // Alınan token body 'i çözer.
 	}
